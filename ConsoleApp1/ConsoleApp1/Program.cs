29d42864dd4b567fbea5c733b0caaa00a0af4c8c
Daniel Sebastián Vizgarra Livón <52511637+dsvlivon@users.noreply.github.com>
2022-03-23 09:58:44 -0300

    Add files via upload
    C# Getting started by paolo perrotta

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyNamespace
+{
+    class MyClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola gaturro!");
+
+        }
+    }
+
+}
